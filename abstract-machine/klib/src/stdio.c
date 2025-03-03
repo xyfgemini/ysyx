6cbf53e08798fbf1c0311040e56911ccc0789145
xyfgemini <xyf2576131943@outlook.com>
2025-03-03 21:29:53 +0800

    update

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -18,32 +18,52 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 int sprintf(char *out, const char *fmt, ...) {
     va_list args;
     char ch;
-    int count = 0,length = 0;
+    char *buf;
+    char buf_tmp[512];
+    char *string_tmp;
+    int int_tmp;
+    int count = 0;
+    int length = 0;
+
     va_start(args,fmt);
+
     while(ch = *fmt++) {
-        //常规
-        *out++ = ch;
+        *buf++ = ch;
         ++count;
         //%s %d
         if(ch == '%') {
-            *out++ = ch;
+            *buf++ = ch;
             ++count;
             switch(ch = *fmt++) {
-                case 's':  *out +=  va_arg(args,char*);
-                case 'd':  *out++ = va_arg(args,int);
-                            count += length;
-                            break;
-                default:  break;
+                case 's':  
+                    string_tmp = va_arg(args,char*); 
+                    length = strlen(string_tmp); 
+                    buf = string_tmp;
+                    buf += length; 
+                    count += length;
+                    break;
+
+                case 'd':  
+                    int_tmp = va_arg(args,int); 
+                    itoa(int_tmp,buf_tmp,10);
+                    length = strlen(buf_tmp);
+                    buf = buf_tmp;
+                    buf += length;
+                    count += length;
+                    break;
+
+                default:  
+                    break;
             }
         }  
     }
-    *out = '\0';
+    *buf = '\0';
     va_end(args);
+    out = buf;
     return count;
-    return 0;
 }
 
-//
+
 int snprintf(char *out, size_t n, const char *fmt, ...) {
     return 0;
   
