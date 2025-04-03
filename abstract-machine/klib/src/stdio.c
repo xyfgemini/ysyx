e613cee0cd6d44ed3b72501e70ff10d7be9d1282
xyfgemini <xyf2576131943@outlook.com>
2025-04-04 02:26:56 +0800

    update

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,11 +10,11 @@ char *out_tmp = NULL;
 char num_arr[256];
 char *num_arr_ptr = NULL;
 
-void PrintNum(int d) {
+void PrintDec(int d) {
     if(d == 0) {
         return;
     } else {
-        PrintNum(d/10);
+        PrintDec(d/10);
         //trick:以字符串开头的数组形式的表达式，它会根据数组下表，找到对应字符作为输出
         char ch = "0123456789"[d % 10];
         *num_arr_ptr = ch;
@@ -33,7 +33,7 @@ void dec_display(int d) {
     } else {
         memset(num_arr,'\0',256);
         num_arr_ptr = num_arr;
-        PrintNum(d);
+        PrintDec(d);
         memcpy(out_tmp,num_arr,strlen(num_arr));
         out_tmp += strlen(num_arr);
     }
@@ -44,33 +44,10 @@ void char_display(char *s) {
     out_tmp += strlen(s);
 }
 
-
-
-int printf(const char *fmt, ...) {
-    // va_list args;
-    // va_start(args,fmt);
-    // while(*fmt != '\0') {
-    //     if(*fmt == '%') {
-    //         putstr(va_arg(args,char*));
-    //     } else {
-    //         putch(*fmt);
-    //     }
-    //     fmt++;
-    // }
-    // va_end(args);
-    return 0;
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-    return 0;
-
-}
-
 int sprintf(char *out, const char *fmt, ...) {
     va_list args;
     va_start(args,fmt);
     char ch;
-    //
     memset(out,'\0',128);
     out_tmp = out;
     while((ch = *fmt) != '\0') {
@@ -92,6 +69,43 @@ int sprintf(char *out, const char *fmt, ...) {
     return 0;
 }
 
+void PrintNum(unsigned int d,int base) {
+     if(d == 0) {
+        return;
+    } else {
+        PrintNum(d/base,base);
+        char ch = "0123456789abcdef"[d % base];
+        putch(ch);
+    }    
+}
+
+int printf(const char *fmt, ...) {
+    va_list args;
+    va_start(args,fmt);
+    while(*fmt != '\0') {
+        if(*fmt == '%') {
+            fmt++;
+            switch(*fmt) {
+                case '%': putch(*fmt); break;
+                case 's': putstr(va_arg(args,char*)); break;
+                case 'd': PrintNum(va_arg(args,int),10); break;
+                case 'x': PrintNum(va_arg(args,unsigned int),16); break;
+            }
+        } else {
+            putch(*fmt);
+        }
+        fmt++;
+    }
+    va_end(args);
+    return 0;
+}
+
+
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+    return 0;
+
+}
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
     return 0;
