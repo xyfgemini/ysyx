5888fca38027f4d4b968e467e2860c5c4ee43a9d
xyfgemini <xyf2576131943@outlook.com>
2025-04-06 00:58:05 +0800

    update

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,9 +5,10 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 
-    kbd->keycode = inl(KBD_ADDR) & ~KEYDOWN_MASK;
+    uint32_t k = inl(KBD_ADDR) ;
+    kbd->keycode = k & ~KEYDOWN_MASK;
 
-    kbd->keydown = inl(KBD_ADDR) & KEYDOWN_MASK ? true : false;
+    kbd->keydown = (k & KEYDOWN_MASK ? true : false);
 }
 
 
